8bca579f8015f69d7e3d71c78a950cacdc9ec84c
Hardik72p <hardik.pedhadiya@bacancytechnology.com>
2020-01-27 14:10:07 +0530

    scssTask

--- responsive/src/Header.js
+++ responsive/src/Header.js
@@ -3,7 +3,18 @@ import React from 'react';
 function Header() {
 	return (
 			<div className="header">
-				<h2>Header</h2>
+				<div className="left-header"><h1>header</h1></div>
+				<div className="right-header">
+					<div>
+						<h4>Home</h4>
+					</div>
+					<div>
+						<h4>Menu</h4>
+					</div>
+					<div>
+						<h4>About Us</h4>
+					</div>
+				</div>
 			</div>		
 	);
 }

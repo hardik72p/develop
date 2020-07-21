e8c7646f4dfce7919993fc6f5f6cd0522f1ec2bd
hardik72p <59606681+hardik72p@users.noreply.github.com>
2020-07-21 11:24:48 +0530

    Update PrivateRoute.js

--- login-local-storage/src/PrivateRoute.js
+++ login-local-storage/src/PrivateRoute.js
@@ -4,9 +4,39 @@ import DashBoard from './DashBoard';
 import LogIn from './LogIn';
 
 function PrivateRoute(props) {		
-			if(localStorage.getItem("id") != null)
-					return <Route  path={props.path} component={props.component} />
-			
+				if (localStorage.getItem("st")) {
+		let time = (new Date().getTime() - new Date(localStorage.getItem("st")).getTime()) / 60000
+		localStorage.removeItem('st');
+		if (localStorage.getItem("id") != null && (time <= 1)) {
+			return (
+				<>
+					{window.addEventListener("beforeunload", () => {
+						// setInterval(() => {
+						localStorage.setItem("st", new Date());
+						// }, 1000);
+					})}
+					<Route path={props.path} component={props.component} />
+				</>
+			)
+		}
+		return (
+			<>
+				{localStorage.removeItem('id')} //logout code
+				<Redirect to="/login" />
+			</>
+		)
+	}
+	if (localStorage.getItem("id") != null)
+		return (
+			<>
+				{window.addEventListener("beforeunload", () => {
+					// setInterval(() => {
+					localStorage.setItem("st", new Date());
+					// }, 1000);
+				})}
+				<Route path={props.path} component={props.component} />
+			</>
+		)
 			return <Redirect to="/login" />
 }
 
